9b800213686ccbd324dfed964a6a787418a81ca0
lealemb <lealemb84@gmail.com>
2024-10-31 02:19:17 +0300

    Footer and Header Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,59 @@
 import React from 'react'
+import "./footer.css";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
-export const Footer = () => {
+const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-outer-container">
+      <div className="footer-inner-container">
+        <div className="footer-icons">
+          <FacebookIcon />
+          <InstagramIcon />
+          <YouTubeIcon />
+        </div>
+        <div className="footer-date">
+          <div>
+            <ul>
+              <li>Audio Description</li>
+              <li>Investor Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+            <li>Help Center</li>
+            <li>Jobs</li>
+            <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+            <li>Gift Cards</li>
+            <li>Terms Of Use</li>
+            <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div>
+            <ul>
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
+        </div>
+        <div className="service-code">
+          <p>
+            service code
+          </p>
+        </div>
+        <div className="copy-write">
+          &copy; 1997-2021 Netflix, Inc.
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default Footer
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import "./header.css";
-export const Header = () => {
+import NetflixLogo from '../../assets/images/Netflix1Logo.png';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+const Header = () => {
   return (
     <div className='header-outer-container'>
         <div className='header-container'>
             <div className='header-left'>
                 <ul>
-                    {/* {#image} */}
+                    <li><img src={NetflixLogo} alt="NetflixLogo" width="100" /></li>
                     <li>Netflix</li>
                     <li>Home</li>
                     <li>TvShow</li>
@@ -18,13 +23,15 @@ export const Header = () => {
             </div>
             <div className='header-right'>
                 <ul>
-                {/* <li>SearchIcon</li>
-                <li>NotificationsNoneIcon</li>
-                <li>AccountBoxIcon</li>
-                <li>ArrowDropDownIcon</li> */}
+                    <li><SearchIcon/></li>
+                    <li><NotificationsNoneIcon/></li>
+                    <li><AccountBoxIcon/></li>
+                    <li><ArrowDropDownIcon/></li>
                 </ul>
             </div>
         </div>
     </div>
   )
 }
+
+export default Header
